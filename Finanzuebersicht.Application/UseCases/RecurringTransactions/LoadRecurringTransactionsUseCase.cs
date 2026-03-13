eda25cef834ed814452f26bdf552029a3872e540
Thomas Menzl <n8-h4wk@msn.com>
2026-03-13 19:16:44 +0100

    🔧 refactor(LoadRecurringTransactionsUseCase): simplify constructor by using parameter assignment

--- Finanzuebersicht.Application/UseCases/RecurringTransactions/LoadRecurringTransactionsUseCase.cs
+++ Finanzuebersicht.Application/UseCases/RecurringTransactions/LoadRecurringTransactionsUseCase.cs
@@ -3,18 +3,13 @@ using Finanzuebersicht.Services;
 
 namespace Finanzuebersicht.Application.UseCases.RecurringTransactions;
 
-public class LoadRecurringTransactionsUseCase
+public class LoadRecurringTransactionsUseCase(IRecurringTransactionRepository recurringTransactionRepository)
 {
-    private readonly IRecurringTransactionRepository _recurringTransactionRepository;
-
-    public LoadRecurringTransactionsUseCase(IRecurringTransactionRepository recurringTransactionRepository)
-    {
-        _recurringTransactionRepository = recurringTransactionRepository;
-    }
+    private readonly IRecurringTransactionRepository _recurringTransactionRepository = recurringTransactionRepository;
 
     public async Task<List<RecurringTransaction>> ExecuteAsync()
     {
         var liste = await _recurringTransactionRepository.GetRecurringTransactionsAsync();
-        return liste.OrderByDescending(d => d.Aktiv).ThenBy(d => d.Titel).ToList();
+        return [.. liste.OrderByDescending(d => d.Aktiv).ThenBy(d => d.Titel)];
     }
 }
